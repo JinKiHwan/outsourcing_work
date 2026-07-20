fd27503f0b34f06d172280a570496d09aa20ebcd
kihwan <eljsh95@naver.com>
2026-07-20 22:15:09 +0900

    마크업 완료

--- 260720/icl_01/pc/resources/js/common.js
+++ 260720/icl_01/pc/resources/js/common.js
@@ -3,7 +3,14 @@ $(function () {
 
     // inc05_scripts li 클릭 시 active 토글 및 형제 li active 제거
     $('.inc05_scripts ul li').on('click', function () {
+        var idx = $(this).index();
         $(this).addClass('active').siblings().removeClass('active');
+        
+        if (idx === 1) {
+            $('.inc05_item').addClass('active');
+        } else {
+            $('.inc05_item').removeClass('active');
+        }
     });
 
     // inc06_items li 클릭 시 active 토글 및 형제 li active 제거
