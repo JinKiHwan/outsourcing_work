96ee75314c30a00f58490ed51a6225d2834fd826
kihwan <eljsh95@naver.com>
2026-07-21 14:07:15 +0900

    재수술 완료

--- 260720/icl_01/pc/resources/js/common.js
+++ 260720/icl_01/pc/resources/js/common.js
@@ -1,5 +1,10 @@
 $(function () {
-    AOS.init();
+    // AOS 옵션 통일 설정 (속도: 800ms, 감속: ease-out-cubic)
+    AOS.init({
+        duration: 500,
+        easing: 'ease-out-cubic',
+        once: true
+    });
 
     // inc05_scripts li 클릭 시 active 토글 및 형제 li active 제거
     $('.inc05_scripts ul li').on('click', function () {
